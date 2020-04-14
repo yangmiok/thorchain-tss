862252c8de2acc601a89a037670687d50d8b03ae
Heimdall Thor <heimdallthor02@gmail.com>
2020-04-15 09:28:23 +1000

    94-issue stop hash the payload

--- common/tss_helper.go
+++ common/tss_helper.go
@@ -71,12 +71,7 @@ func SetupIDMaps(parties map[string]*btss.PartyID, partyIDtoP2PID map[string]pee
 }
 
 func MsgToHashInt(msg []byte) (*big.Int, error) {
-	h := sha256.New()
-	_, err := h.Write(msg)
-	if err != nil {
-		return nil, fmt.Errorf("fail to caculate sha256 hash: %w", err)
-	}
-	return hashToInt(h.Sum(nil), btcec.S256()), nil
+	return hashToInt(msg, btcec.S256()), nil
 }
 
 func MsgToHashString(msg []byte) (string, error) {
@@ -328,7 +323,6 @@ func (t *TssCommon) GetUnicastBlame(msgType string) ([]string, error) {
 }
 
 func (t *TssCommon) GetBroadcastBlame(lastMessageType string) ([]string, error) {
-
 	localCachedItems := t.TryGetAllLocalCached()
 	blamePeers, err := t.TssTimeoutBlame(localCachedItems, lastMessageType)
 	if err != nil {
